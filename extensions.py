4f62e82aa69aba9beffca8ff9c6939470061945c
Arjun <deloterio19@gmail.com>
2026-08-10 19:27:09 +0800

    Include the Initial FIll to the Total amount to pay

--- extensions.py
+++ extensions.py
@@ -19,7 +19,7 @@ mail = Mail()
 csrf = CSRFProtect()
 limiter = Limiter(
     key_func=get_remote_address,
-    # storage_uri="redis://redis:6379/0",
+    storage_uri="redis://redis:6379/0",
     default_limits=["200 per day", "50 per hour"]
 )
 login_manager.login_view = "auth.login"
